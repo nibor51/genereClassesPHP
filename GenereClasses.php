c054799052187377311b77c4483fdaba45902c0b
nibor51 <jonval.robin@gmail.com>
2023-02-15 07:00:30 +0100

    add GenereClasses.php and comment for future code

---
+++ GenereClasses.php
@@ -0,0 +1,23 @@
+<?php
+
+// recup parametre de connexion
+// connexion BDD
+// verif connexion ok
+
+
+//recup schema BDD
+
+// debut boucle sur chaque table
+
+    // récupération du schéma de la table
+
+    // debut boucle sur chaque colonne d'une table
+
+        // génération de la classe
+        
+    // fin boucle sur chaque colonne d'une table
+
+    //ajout des methodes
+    // création fichier class
+
+// fin boucle sur chaque table
