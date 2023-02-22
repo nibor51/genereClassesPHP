dbc9558a01b0eaac20d693d7770404eb31f7d509
nibor51 <jonval.robin@gmail.com>
2023-02-22 12:01:23 +0100

    used correct global

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once '_connec.php';
 require_once 'GenereClasses.php';
 
-$portfolioClasses = new GenereClasses(DB_NAME, DB_HOST, DB_NAME, DB_PASSWORD);
+$portfolioClasses = new GenereClasses(DB_NAME, DB_HOST, DB_USER, DB_PASSWORD);
 $portfolioClasses->generateClasses();
 
 
