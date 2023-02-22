3921578e4b3f77a6ce109093c24b2d2e720e98ca
nibor51 <jonval.robin@gmail.com>
2023-02-22 11:53:39 +0100

    moved private function under public function

--- GenereClasses.php
+++ GenereClasses.php
@@ -32,121 +32,6 @@ class GenereClasses
         }
     }
 
-    private function initialGeneration() {
-        // create a folder for config files
-        if(!file_exists('config')) {
-            mkdir('config');
-        }
-        // generate config.php
-        if(!file_exists('config/config.php')) {
-            $config = fopen('config/config.php', 'w');
-            $content = "<?php\n\n";
-            $content .= "require_once 'db.php';\n\n";
-            $content .= "define('ENV', getenv('ENV') ? getenv('ENV') : 'dev');\n\n";
-            $content .= "//Model (for connexion data, see unversionned db.php)\n";
-            $content .= "define('DB_USER', getenv('DB_USER') ? getenv('DB_USER') : APP_DB_USER);\n";
-            $content .= "define('DB_PASSWORD', getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : APP_DB_PASSWORD);\n";
-            $content .= "define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : APP_DB_HOST);\n";
-            $content .= "define('DB_NAME', getenv('DB_NAME') ? getenv('DB_NAME') : APP_DB_NAME);\n";
-            fwrite($config, $content);
-            fclose($config);
-        }
-
-        // generate db.php.dist if not exist and add db.php to .gitignore
-        if(!file_exists('config/db.php.dist')) {
-            $connect = fopen('config/db.php.dist', 'w');
-            $content = "<?php\n\n";
-            $content .= "define('APP_DB_USER', 'root');\n";
-            $content .= "define('APP_DB_PASSWORD', 'root');\n";
-            $content .= "define('APP_DB_HOST', 'localhost');\n";
-            $content .= "define('APP_DB_NAME', 'database_Name');\n";
-            fwrite($connect, $content);
-            fclose($connect);
-            $gitignore = fopen('.gitignore', 'a');
-            fwrite($gitignore, "\ndb.php");
-            fclose($gitignore);
-        }
-        // create a folder for the models if not exist
-        if(!file_exists('src/Model')) {
-            mkdir('src/Model', 0777, true);
-        }
-
-        // generate the connection.php file if not exist
-        if(!file_exists('src/Model/connection.php')) {
-            $connection = fopen('src/Model/connection.php', 'w');
-            $content = "<?php\n\n";
-            $content .= "require_once 'config/config.php';\n\n";
-            $content .= "    class Connection {\n\n";
-            $content .= "        private PDO \$pdoConnection;\n\n";
-            $content .= "        private string \$user;\n\n";
-            $content .= "        private string \$host;\n\n";
-            $content .= "        private string \$password;\n\n";
-            $content .= "        private string \$dbName;\n\n";
-            $content .= "        public function __construct() {\n";
-            $content .= "            \$this->user = DB_USER;\n";
-            $content .= "            \$this->host = DB_HOST;\n";
-            $content .= "            \$this->password = DB_PASSWORD;\n";
-            $content .= "            \$this->dbName = DB_NAME;\n";
-            $content .= "            try {\n";
-            $content .= "                \$this->pdoConnection = new PDO(\n";
-            $content .= "                'mysql:host=' . \$this->host . '; dbname=' . \$this->dbName . '; charset=utf8',\n";
-            $content .= "                \$this->user,\n";
-            $content .= "                \$this->password\n";
-            $content .= "               );\n";
-            $content .= "               \$this->pdoConnection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);\n\n";
-            $content .= "               // show errors in DEV environment\n";
-            $content .= "               if (ENV === 'dev') {\n";
-            $content .= "                   \$this->pdoConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);\n";
-            $content .= "               }\n";
-            $content .= "           } catch (PDOException \$e) {\n";
-            $content .= "               echo \"\n";
-            $content .= "                   Erreur de connexion à la base de données \n";
-            $content .= "                   Message d'erreur : \".\$e->getMessage().\"\n";
-            $content .= "               \";\n";
-            $content .= "           };\n\n";
-            $content .= "        }\n\n";
-            $content .= "        public function getPdoConnection(): PDO {\n";
-            $content .= "            return \$this->pdoConnection;\n";
-            $content .= "        }\n}";
-            fwrite($connection, $content);
-            fclose($connection);
-        }
-
-        // generate the AbstractManager.php file if not exist
-        if(!file_exists('src/Model/AbstractManager.php')) {
-            $abstractManager = fopen('src/Model/AbstractManager.php', 'w');
-            $content = "<?php\n\n";
-            $content .= "require_once 'connection.php';\n\n";
-            $content .= "    abstract class AbstractManager {\n\n";
-            $content .= "        protected PDO \$pdo;\n\n";
-            $content .= "        public const TABLE = '';\n\n";
-            $content .= "        public function __construct() {\n";
-            $content .= "            \$connection = new Connection();\n";
-            $content .= "            \$this->pdo = \$connection->getPdoConnection();\n";
-            $content .= "        }\n\n";
-            $content .= "        public function selectAll(): array {\n";
-            $content .= "            \$query = 'SELECT * FROM ' . static::TABLE;\n";
-            $content .= "            \$statement = \$this->pdo->query(\$query);\n";
-            $content .= "            return \$statement->fetchAll();\n";
-            $content .= "        }\n\n";
-            $content .= "        public function selectOneById(int \$id): array {\n";
-            $content .= "            \$query = 'SELECT * FROM ' . static::TABLE . ' WHERE id = :id';\n";
-            $content .= "            \$statement = \$this->pdo->prepare(\$query);\n";
-            $content .= "            \$statement->bindValue('id', \$id, PDO::PARAM_INT);\n";
-            $content .= "            \$statement->execute();\n";
-            $content .= "            return \$statement->fetch();\n";
-            $content .= "        }\n\n";
-            $content .= "        public function delete(int \$id): void {\n";
-            $content .= "            \$query = 'DELETE FROM ' . static::TABLE . ' WHERE id = :id';\n";
-            $content .= "            \$statement = \$this->pdo->prepare(\$query);\n";
-            $content .= "            \$statement->bindValue('id', \$id, PDO::PARAM_INT);\n";
-            $content .= "            \$statement->execute();\n";
-            $content .= "        }\n\n}\n";
-            fwrite($abstractManager, $content);
-            fclose($abstractManager);
-        }
-    }
-
     public function generateClasses() {
         //connection to database
         $pdo = $this->pdoConnection;
@@ -317,6 +202,121 @@ class GenereClasses
             $file = fopen('src/Model/'.$className.'.php', 'w+');
             fwrite($file, $class);
             fclose($file);
+        }
+    }
+
+    private function initialGeneration() {
+        // create a folder for config files
+        if(!file_exists('config')) {
+            mkdir('config');
+        }
+        // generate config.php
+        if(!file_exists('config/config.php')) {
+            $config = fopen('config/config.php', 'w');
+            $content = "<?php\n\n";
+            $content .= "require_once 'db.php';\n\n";
+            $content .= "define('ENV', getenv('ENV') ? getenv('ENV') : 'dev');\n\n";
+            $content .= "//Model (for connexion data, see unversionned db.php)\n";
+            $content .= "define('DB_USER', getenv('DB_USER') ? getenv('DB_USER') : APP_DB_USER);\n";
+            $content .= "define('DB_PASSWORD', getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : APP_DB_PASSWORD);\n";
+            $content .= "define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : APP_DB_HOST);\n";
+            $content .= "define('DB_NAME', getenv('DB_NAME') ? getenv('DB_NAME') : APP_DB_NAME);\n";
+            fwrite($config, $content);
+            fclose($config);
+        }
+
+        // generate db.php.dist if not exist and add db.php to .gitignore
+        if(!file_exists('config/db.php.dist')) {
+            $connect = fopen('config/db.php.dist', 'w');
+            $content = "<?php\n\n";
+            $content .= "define('APP_DB_USER', 'root');\n";
+            $content .= "define('APP_DB_PASSWORD', 'root');\n";
+            $content .= "define('APP_DB_HOST', 'localhost');\n";
+            $content .= "define('APP_DB_NAME', 'database_Name');\n";
+            fwrite($connect, $content);
+            fclose($connect);
+            $gitignore = fopen('.gitignore', 'a');
+            fwrite($gitignore, "\ndb.php");
+            fclose($gitignore);
+        }
+        // create a folder for the models if not exist
+        if(!file_exists('src/Model')) {
+            mkdir('src/Model', 0777, true);
+        }
+
+        // generate the connection.php file if not exist
+        if(!file_exists('src/Model/connection.php')) {
+            $connection = fopen('src/Model/connection.php', 'w');
+            $content = "<?php\n\n";
+            $content .= "require_once 'config/config.php';\n\n";
+            $content .= "    class Connection {\n\n";
+            $content .= "        private PDO \$pdoConnection;\n\n";
+            $content .= "        private string \$user;\n\n";
+            $content .= "        private string \$host;\n\n";
+            $content .= "        private string \$password;\n\n";
+            $content .= "        private string \$dbName;\n\n";
+            $content .= "        public function __construct() {\n";
+            $content .= "            \$this->user = DB_USER;\n";
+            $content .= "            \$this->host = DB_HOST;\n";
+            $content .= "            \$this->password = DB_PASSWORD;\n";
+            $content .= "            \$this->dbName = DB_NAME;\n";
+            $content .= "            try {\n";
+            $content .= "                \$this->pdoConnection = new PDO(\n";
+            $content .= "                'mysql:host=' . \$this->host . '; dbname=' . \$this->dbName . '; charset=utf8',\n";
+            $content .= "                \$this->user,\n";
+            $content .= "                \$this->password\n";
+            $content .= "               );\n";
+            $content .= "               \$this->pdoConnection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);\n\n";
+            $content .= "               // show errors in DEV environment\n";
+            $content .= "               if (ENV === 'dev') {\n";
+            $content .= "                   \$this->pdoConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);\n";
+            $content .= "               }\n";
+            $content .= "           } catch (PDOException \$e) {\n";
+            $content .= "               echo \"\n";
+            $content .= "                   Erreur de connexion à la base de données \n";
+            $content .= "                   Message d'erreur : \".\$e->getMessage().\"\n";
+            $content .= "               \";\n";
+            $content .= "           };\n\n";
+            $content .= "        }\n\n";
+            $content .= "        public function getPdoConnection(): PDO {\n";
+            $content .= "            return \$this->pdoConnection;\n";
+            $content .= "        }\n}";
+            fwrite($connection, $content);
+            fclose($connection);
+        }
+
+        // generate the AbstractManager.php file if not exist
+        if(!file_exists('src/Model/AbstractManager.php')) {
+            $abstractManager = fopen('src/Model/AbstractManager.php', 'w');
+            $content = "<?php\n\n";
+            $content .= "require_once 'connection.php';\n\n";
+            $content .= "    abstract class AbstractManager {\n\n";
+            $content .= "        protected PDO \$pdo;\n\n";
+            $content .= "        public const TABLE = '';\n\n";
+            $content .= "        public function __construct() {\n";
+            $content .= "            \$connection = new Connection();\n";
+            $content .= "            \$this->pdo = \$connection->getPdoConnection();\n";
+            $content .= "        }\n\n";
+            $content .= "        public function selectAll(): array {\n";
+            $content .= "            \$query = 'SELECT * FROM ' . static::TABLE;\n";
+            $content .= "            \$statement = \$this->pdo->query(\$query);\n";
+            $content .= "            return \$statement->fetchAll();\n";
+            $content .= "        }\n\n";
+            $content .= "        public function selectOneById(int \$id): array {\n";
+            $content .= "            \$query = 'SELECT * FROM ' . static::TABLE . ' WHERE id = :id';\n";
+            $content .= "            \$statement = \$this->pdo->prepare(\$query);\n";
+            $content .= "            \$statement->bindValue('id', \$id, PDO::PARAM_INT);\n";
+            $content .= "            \$statement->execute();\n";
+            $content .= "            return \$statement->fetch();\n";
+            $content .= "        }\n\n";
+            $content .= "        public function delete(int \$id): void {\n";
+            $content .= "            \$query = 'DELETE FROM ' . static::TABLE . ' WHERE id = :id';\n";
+            $content .= "            \$statement = \$this->pdo->prepare(\$query);\n";
+            $content .= "            \$statement->bindValue('id', \$id, PDO::PARAM_INT);\n";
+            $content .= "            \$statement->execute();\n";
+            $content .= "        }\n\n}\n";
+            fwrite($abstractManager, $content);
+            fclose($abstractManager);
         }
     }
 
