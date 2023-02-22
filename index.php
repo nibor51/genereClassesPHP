6cf1048deaa2f0eb1f06af0b9a621765c3737c46
nibor51 <jonval.robin@gmail.com>
2023-02-22 12:19:18 +0100

    moved test to separate file

--- index.php
+++ index.php
@@ -5,16 +5,3 @@ require_once 'GenereClasses.php';
 
 $portfolioClasses = new GenereClasses(DB_NAME, DB_HOST, DB_USER, DB_PASSWORD);
 $portfolioClasses->generateClasses();
-
-
-// Example of use, replace by your own class name
-
-require_once 'src/Model/TechManager.php';
-$techManager = new TechManager();
-
-print_r($techManager->selectAll());
-$techManager->add('test', 'test');
-print_r($techManager->search('test'));
-
-
-// $techManager->delete(18);
